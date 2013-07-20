c1d021f4b9426b42a460cb60b269dec3efa9dad2
dr_bonzo <dr_bonzo@northslope.pl>
2013-07-20 16:59:35 +0200

    Vat Rates summary table

--- js/invoices.js
+++ js/invoices.js
@@ -1,11 +1,11 @@
 function InvoiceController($scope) {
 
     $scope.vatRates = [
-        { label: "23 %", id: 23, value: 0.23 },
-        { label: "8 %", id: 8, value: 0.08 },
-        { label: "5 %", id: 3, value: 0.05 },
-        { label: "0 %", id: 0, value: 0.0 },
-        { label: "zw.", id: -1, value: 0.0 }
+        { label: '23%', id: 'rate_23', value: 0.23 },
+        { label: '8%', id: 'rate_8', value: 0.08 },
+        { label: '5%', id: 'rate_3', value: 0.05 },
+        { label: '0%', id: 'rate_0', value: 0.0 },
+        { label: 'zw.', id: 'rate_zw', value: 0.0 }
     ];
 
     var defaultVatRate = $scope.vatRates[0];
@@ -30,10 +30,36 @@ function InvoiceController($scope) {
     $scope.addNewInvoiceItem = function () {
         $scope.invoiceItems.push(new InvoiceItem(null, 0, 1, defaultVatRate));
         // needs to be called "later" as it will not work
-        setTimeout(function() {
+        setTimeout(function () {
             $('.invoiceItems tr.invoiceItem:last input:first').focus();
         }, 10);
     };
+
+    $scope.vatSummaryRows = [];
+    for (var i in $scope.vatRates) {
+        // todo index with vat.id
+        var vatSummaryRow = new VatSummaryRow($scope.vatRates[i]);
+        $scope.vatSummaryRows.push(vatSummaryRow);
+    }
+
+    // FIXME crete invoice, adding, removing items will refresh it
+    // FIXME what about invoice item changes?
+    $scope.getVatSummaryRows = function () {
+        for (var i in $scope.vatSummaryRows) {
+            var vsr = $scope.vatSummaryRows[i];
+            vsr.reset();
+
+            for (var ii in $scope.invoiceItems) {
+                var invoiceItem = $scope.invoiceItems[ii];
+                if (invoiceItem.vatRate === vsr.vatRate) {
+                    vsr.addInvoiceItem(invoiceItem);
+                }
+            }
+
+            vsr.refreshTotals();
+        }
+        return $scope.vatSummaryRows;
+    }
 }
 
 function InvoiceItem(name, unitNettoPrice, quantity, vatRate) {
@@ -69,4 +95,34 @@ InvoiceItem.prototype.totalBruttoPrice = function () {
 
 InvoiceItem.prototype.roundPrice = function (price) {
     return Number((price.toFixed(2)));
+};
+
+function VatSummaryRow(vatRate) {
+    this.vatRate = vatRate;
+    this.totalNettoValue = 0.0;
+    this.totalVatValue = 0.0;
+    this.totalBruttoValue = 0.0;
+
+    this.reset();
+}
+
+VatSummaryRow.prototype.reset = function () {
+    this.totalNettoValue = 0.0;
+    this.totalVatValue = 0.0;
+    this.totalBruttoValue = 0.0;
+};
+
+
+VatSummaryRow.prototype.roundPrice = function (price) {
+    return Number((price.toFixed(2)));
+};
+
+VatSummaryRow.prototype.addInvoiceItem = function (invoiceItem) {
+    this.totalNettoValue += invoiceItem.totalNettoPrice();
+};
+
+VatSummaryRow.prototype.refreshTotals = function () {
+    this.totalNettoValue = this.roundPrice(this.totalNettoValue);
+    this.totalVatValue = this.roundPrice(this.totalNettoValue * this.vatRate.value);
+    this.totalBruttoValue = this.roundPrice(this.totalNettoValue + this.totalVatValue);
 };
